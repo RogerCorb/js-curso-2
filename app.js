6e36e66f905acdd2e57c2c312a6b7a12ef9ce83c
RogerCorb <rogeriocgprest@gmail.com>
2024-01-21 20:36:29 -0300

    desafio resolvido

---
+++ app.js
@@ -0,0 +1,15 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do desafio';
+
+function verificarChute() {
+    console.log('O botão foi clicado');  
+}
+
+function fraseAlerta() {
+    console.log('Eu amo Js');    
+}
+
+function concatenaPrompt() {
+    let cidade = prompt('Digite o nome de uma cidade');
+    alert(`Estive em ${cidade} e lembrei de você`);    
+}
